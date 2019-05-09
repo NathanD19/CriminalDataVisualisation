0c453da0c9cb1c4b514dac845d4a6e19de8b19e2
NathanD19 <nathandavies19@yahoo.com.au>
2019-05-09 22:02:43 +1000

    Updated csv files for crime heading Created Simple Tooltip

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,8 @@
 function init() {
-    geomap();
+    geomap("data/LGA-data-2017.csv");
 }
 
-function geomap() {
+function geomap(crimedata) {
 
     var w = 900;
     var h = 600;
@@ -15,20 +15,29 @@ function geomap() {
 
     var path = d3.geoPath().projection(projection);
     //["#d3efff","#99d6ff", "#4db8ff", "#008ae6", "#005c99"]
-    var color = d3.scaleQuantile().range(["#d3efff","#4db8ff","#005c99"]);
+    var color = d3.scaleQuantile().range(["#d3efff", "#4db8ff", "#005c99"]);
 
     //var color = d3.scaleLinear().range(["Orange", "Red"]).interpolate(d3.interpolateLab);
-  
+
     var svg = d3.select("#chart")
         .append("svg")
         .attr("width", w)
         .attr("height", h);
 
-    d3.csv("data/LGA-data-2017.csv").then(function (data) {
+    var tooltip = d3.select("#chart").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
+    // d3.select("prev").on("click", function(){
+
+    // });
+
+    d3.csv(crimedata).then(function (data) {
 
         console.log(data)
-        color.domain([d3.min(data, function (d) { return d.unemployed; }),
-        d3.max(data, function (d) { return d.unemployed; })
+        color.domain([d3.min(data, function (d) { return d.crimes; }),
+        d3.max(data, function (d) { return d.crimes; })
         ]);
 
         d3.json("data/LGA_VIC.json").then(function (json) {
@@ -36,7 +45,7 @@ function geomap() {
             for (var i = 0; i < data.length; i++) {
                 var dataState = data[i].LGA;
 
-                var dataValue = parseFloat(data[i].unemployed);
+                var dataValue = parseFloat(data[i].crimes);
 
                 for (var j = 0; j < json.features.length; j++) {
                     var jsonState = json.features[j].properties.LGA_name;
@@ -54,6 +63,19 @@ function geomap() {
                 .append("path")
                 .attr("stroke-width", 1.25)
                 .attr("d", path)
+                .on("mouseover", function (d) {
+                    tooltip.transition()
+                        .duration(250)
+                        .style("opacity", 1);
+                    tooltip.html(d.properties.LGA_name + "<br>Crimes: " + d.properties.value)
+                        .style("left", (d3.event.pageX + 15) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function (d) {
+                    tooltip.transition()
+                        .duration(250)
+                        .style("opacity", 0);
+                })
                 .style("fill", function (d) {
                     //get data
                     var value = d.properties.value;
@@ -68,14 +90,14 @@ function geomap() {
             alert(error);
         });
 
-
     }).catch(function (error) {
         alert(error);
     });
 
 }
 
-
+function ChangePrev() {
+}
 
 window.onload = init;
 
